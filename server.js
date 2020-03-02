accb7b3035bbd232ea4a82a4d586515e1c7eef0a
Marcia <marciaadler718@gmail.com>
2020-03-02 15:39:07 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.static("client/build"));
+
+app.listen(PORT, () => {
+  console.log(`listenting on port ${PORT}`);
+});
